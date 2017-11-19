e65528b0a38159002b7ed88b33f7b20fb9490913
Dmitry <gorbunov.webdev@gmail.com>
2017-11-19 16:13:27 +0400

    Добавляет вариант из демки

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,14 @@
+// stats.js
+
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)'; // white;
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
 
-  ctx.fillStyle = '#000';
+
+  ctx.fillStyle = '#000'; // black;
   ctx.font = '14px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
@@ -13,7 +16,7 @@ window.renderStatistics = function (ctx, names, times) {
   var max = -1;
   var maxIndex = -1;
 
-  for(var i = 0; i < times.length; i++) {
+  for (var i = 0 ; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
@@ -21,12 +24,19 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
-  var histogramWidht = 150;
-  var step = histogramWidht / (max - 0);
+  var histogramWidth = 150;              // px;
+  var step = histogramWidth / (max - 0); // px;
 
   ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 60);
 
-  ctx.fillRect(120, 80, times[0] * step, 20);
-  ctx.fillRect(120, 110, times[1] * step, 20);
-  ctx.fillRect(120, 140, times[2] * step, 20);
+  var barHeigth = 20; // px;
+  var indent = 40;    // px;
+  var initialX = 120; // px;
+  var initialY = 80;  // px;
+  var lineHeight = 15;// px;
+
+  for(var i = 0; i < times.length; i++) {
+    ctx.fillRect(initialX, initialY + indent * i, times[i] * step, barHeigth);
+    ctx.fillText(names[i], initialX + histogramWidth, initialY + lineHeight + indent * i);
+  }
 };
